9e203709691a2fd6d501664c7d915ac1408cc373
skunshd <skunshd@gmail.com>
2018-07-05 13:01:21 +0300

    add prod mode

--- webpack.config.js
+++ webpack.config.js
@@ -5,45 +5,50 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 
-module.exports = {
-    entry: {
-        app: './src/index.js'
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-        hot: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    // creates style nodes from JS strings
-                    process.env.NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
-                    "css-loader", // translates CSS into CommonJS
-                    {
-                        loader: "sass-loader", options: {
-                            sourceMap: true
-                        } // compiles Sass to CSS
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: "[id].css"
-        }),
-        new HtmlWebpackPlugin({
-            title: 'Hot Module Replacement'
-        }),
-        new webpack.HotModuleReplacementPlugin()
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
+module.exports = function(env, argv) {
+    return {
+        mode: env.production ? 'production' : 'development',
+        entry: {
+            app: './src/index.js'
+        },
+        devtool: env.production ? 'source-maps' : 'eval',
+        devServer: {
+            contentBase: './dist',
+            hot: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    use: [
+                        // creates style nodes from JS strings
+                        env.production ? MiniCssExtractPlugin.loader : 'style-loader',
+                        "css-loader", // translates CSS into CommonJS
+                        {
+                            loader: "sass-loader", // compiles Sass to CSS
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new CleanWebpackPlugin(['dist']),
+
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+                chunkFilename: "[id].css",
+            }),
+            new HtmlWebpackPlugin({
+                title: 'Hot Module Replacement'
+            }),
+            new webpack.HotModuleReplacementPlugin()
+        ],
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist')
+        },
+    };
 };
