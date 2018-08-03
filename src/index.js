ba2f0035ef2ce63b3bf2a60309c20540e48fbf79
malyutenko.d <malyutenko.d@gmail.com>
2018-08-03 14:11:19 +0300

    add svg

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import './styles/main.scss';
 import './js/navigation.js';
 import registerServiceWorker from './js/register-service-worker.js';
 
-registerServiceWorker();
+// registerServiceWorker();
