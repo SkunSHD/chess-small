f0da7f3ec980fa5b848e2e1b3cee17bce39f5e67
skunshd <skunshd@gmail.com>
2018-08-06 10:52:28 +0300

    add active navLink

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,19 +1,29 @@
-(function () {
-    // click handle
-    function onMenuClickToggleHandler(event) {
-        document.querySelector('.nav-list').classList.toggle('visible');
-    }
-    document.getElementById("collapse-toggle").onclick = onMenuClickToggleHandler;
+// click handle
+function onMenuClickToggleHandler(event) {
+    document.querySelector('.nav-list').classList.toggle('visible');
+}
+document.getElementById("collapse-toggle").onclick = onMenuClickToggleHandler;
 
-    // onScroll
-    function onScrollMenuCollapseHandler(event) {
-        const isTop = document.documentElement.scrollTop === 0;
-        const isMobile = document.documentElement.clientWidth < 768;
+// onScroll
+function onScrollMenuCollapseHandler(event) {
+    const isTop = document.documentElement.scrollTop === 0;
+    const isMobile = document.documentElement.clientWidth < 768;
 
-        const normalHeight = isMobile ? '60px' : '70px';
-        const collapsedHeight = isMobile ? '50px' : '50px';
+    const normalHeight = isMobile ? '60px' : '70px';
+    const collapsedHeight = isMobile ? '50px' : '50px';
 
-        document.querySelector('.navigation').style.height = isTop ? normalHeight : collapsedHeight;
-    }
-    document.body.onscroll = onScrollMenuCollapseHandler;
-})();
+    document.querySelector('.navigation').style.height = isTop ? normalHeight : collapsedHeight;
+}
+document.body.onscroll = onScrollMenuCollapseHandler;
+
+
+function onNavClick(e) {
+    const links = document.querySelectorAll('.nav-list ul > li a');
+    links.forEach(link => {
+        if(link.classList.contains('active')) {
+            link.classList.remove('active');
+        }
+    });
+    e.target.classList.add('active');
+}
+document.querySelectorAll('.nav-list ul > li').forEach(link => link.onclick = onNavClick);
